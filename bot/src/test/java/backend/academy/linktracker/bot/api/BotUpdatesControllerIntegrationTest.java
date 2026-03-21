83682a9fdbf15433ebc7aef05928aa026dd8ef56
Spirit <157625521+sptlm@users.noreply.github.com>
2026-03-21 22:40:00 +0300

    Add Spring test support for controller tests

--- bot/src/test/java/backend/academy/linktracker/bot/api/BotUpdatesControllerIntegrationTest.java
+++ bot/src/test/java/backend/academy/linktracker/bot/api/BotUpdatesControllerIntegrationTest.java
@@ -7,7 +7,7 @@ import backend.academy.linktracker.bot.service.BotUpdateService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +19,7 @@ class BotUpdatesControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private BotUpdateService botUpdateService;
 
     /**
