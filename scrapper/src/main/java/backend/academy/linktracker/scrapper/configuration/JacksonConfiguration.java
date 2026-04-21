2c82af62c90c0032cb21224ed939c8c07a7e9031
Spirit <157625521+sptlm@users.noreply.github.com>
2026-04-21 18:57:19 +0300

    Add explicit ObjectMapper beans for bot and scrapper

---
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/configuration/JacksonConfiguration.java
@@ -0,0 +1,15 @@
+package backend.academy.linktracker.scrapper.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder().findAndAddModules().build();
+    }
+}
